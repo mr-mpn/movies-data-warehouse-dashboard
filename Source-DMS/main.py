a6be82087348b7aacbb39d9a3abe955c365b5494
Mohammad Pakdast Nobari <135954454+mr-mpn@users.noreply.github.com>
2026-03-27 23:26:31 +0100

    (add): -Logger -Refactor write to db

--- Source-DMS/main.py
+++ Source-DMS/main.py
@@ -2,8 +2,14 @@ import pandas as pd
 from sqlalchemy import create_engine
 import os
 from dotenv import load_dotenv
+import logging
 
 load_dotenv()
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
 
 '''
 This Script would act as the DMS (Database Management System).
@@ -16,14 +22,34 @@ Source dataset = https://www.kaggle.com/datasets/rounakbanik/the-movies-dataset?
 '''
 
 def read_dataset(file_name):
-    return pd.read_csv(f"./Source-DMS/dataset/{file_name}.csv")
+    logger.info(f"Reading dataset : {file_name}")
+    try:
+        df = pd.read_csv(f"./Source-DMS/dataset/{file_name}.csv")
+        logger.info(f"Dataframe has been loaded")
+        return df
+    except Exception as e:
+        logger.warning(f"Error loading the file: {e}")
+
 
 def connect_db():
-    user = os.getenv("POSTGRES_USER")
-    password = os.getenv("POSTGRES_PASSWORD")
-    db = os.getenv("POSTGRES_DB")
-    engine = create_engine(f"postgresql://{user}:{password}@localhost:5432/{db}")
-    return engine
+    logger.info("Connecting to db : raw")
+    try:
+        user = os.getenv("POSTGRES_USER")
+        password = os.getenv("POSTGRES_PASSWORD")
+        db = os.getenv("POSTGRES_DB")
+        engine = create_engine(f"postgresql://{user}:{password}@localhost:5432/{db}")
+        logger.info("Connected to DB ...")
+        return engine
+    except Exception as e:
+        logger.warning(f"Error Connecting to DB : {e}")
+
+def write_to_db(df,table_name, conn):
+    try:
+        logger.info(f"Starting to write to db  , table name = {table_name}")
+        df.to_sql(table_name, conn, if_exists="replace", index=False)
+        logger.info(f"Data has been inserted to {table_name}")
+    except Exception as e:
+        logger.warning(f"Error writing to DB raw , table name = {table_name} : {e}")
 
 
 if __name__ == "__main__":
@@ -36,7 +62,6 @@ if __name__ == "__main__":
     conn  = connect_db()
 
     # Step 3: Write to the DB
-    ratings.to_sql("ratings_raw", conn, if_exists="replace", index=False)
-    movies.to_sql("movies_raw", conn, if_exists="replace", index=False)
-
+    write_to_db(ratings , 'ratings_raw' , conn)
+    write_to_db(movies , 'movies_raw' , conn)
 
