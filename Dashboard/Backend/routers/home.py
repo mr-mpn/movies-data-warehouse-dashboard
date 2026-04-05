1cbc1f936d1b332ec3667ee494fbd5ce09b79417
Mohammad Pakdast Nobari <135954454+mr-mpn@users.noreply.github.com>
2026-04-05 16:17:03 +0200

    (add) pagination to the home API

--- Dashboard/Backend/routers/home.py
+++ Dashboard/Backend/routers/home.py
@@ -6,14 +6,13 @@ from Dashboard.Backend.DTO.models import HomeResponse
 import pandas as pd
 from typing import List
 
-
 router = APIRouter()
 
 @router.get('/home')
-async def get_Home() -> List[HomeResponse]:
-    conn = connect_db();
-    df = pd.read_sql('''SELECT title , vote_average ,vote_count
-                    FROM rating_movie_silver 
-                    ORDER BY vote_average DESC LIMIT 10''' , conn)
+async def get_Home(page: int = 1, page_size: int = 10) -> List[HomeResponse]:
+    conn = connect_db()
+    offset = (page - 1) * page_size
+    df = pd.read_sql(f'''SELECT title, vote_average, vote_count
+                    FROM rating_movie_silver
+                    ORDER BY vote_average DESC LIMIT {page_size} OFFSET {offset}''', conn)
     return df.to_dict(orient="records")
- 
